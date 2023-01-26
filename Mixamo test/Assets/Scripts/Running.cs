1f2c8d3639fc4280743dd7b8b8172ab254c68726
XuanmingDong <yizi.666@berkeley.edu>
2023-01-26 00:31:45 -0800

    Changed the name of the player to PlayerObj

--- Mixamo test/Assets/Scripts/Running.cs
+++ Mixamo test/Assets/Scripts/Running.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Running : MonoBehaviour
 {
+    public NavMeshAgent agent;
+    public Transform player;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
